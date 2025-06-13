980310f8a5739f270393f3e9ddf56fe13123ba44
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2025-06-13 13:08:35 -0600

    creando formulario para libro

--- src/main/java/Biblioteca/presentacion/libroformC.java
+++ src/main/java/Biblioteca/presentacion/libroformC.java
@@ -6,14 +6,17 @@ public class libroformC extends JDialog {
     private JPanel libropanel;
     private JTextField textField5;
     private JComboBox comboBox1;
-    private JButton button1;
+    private JButton subirLibroButton;
     private JTextField textField6;
     private JTextField textField7;
-    private JButton button2;
+    private JButton selecionarButton;
+    private JButton button1;
+    private JButton crearButton;
+    private JButton cancelarButton;
 
 
     public libroformC(Mainform mainForm){
-        super(mainForm, "Clientes", true); // true para modal
+        super(mainForm, "Crear libro ", true); // true para modal
         setSize(700, 700);
         setLocationRelativeTo(mainForm); // Centra el diálogo respecto a su propietario
         setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);

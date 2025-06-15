ec753645205f5fad87ddac757bde1974ed1a5ad7
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2025-06-15 08:24:51 -0600

    creando vista para el ususario

---
+++ src/main/java/Biblioteca/presentacion/CatalogoForm.java
@@ -0,0 +1,12 @@
+package Biblioteca.presentacion;
+
+import javax.swing.*;
+
+public class CatalogoForm {
+    private JPanel Mainpanel;
+    private JTable table1;
+    private JTextField textField1;
+    private JTable table2;
+    private JButton button1;
+    private JComboBox comboBox2;
+}
